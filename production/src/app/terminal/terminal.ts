e6515651a6afc36c6632f4b41bc58eddbaa1344c
unknown <enzomoreiradasilveira@gmail.com>
2025-10-07 14:16:11 -0300

    Fix folder

--- production/src/app/terminal/terminal.ts
+++ production/src/app/terminal/terminal.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-terminal',
-  imports: [],
-  templateUrl: './terminal.html',
-  styleUrl: './terminal.scss'
+  selector: 'app-terminal', // Nome do componente
+  standalone: true, // Componente independente
+  imports: [], // Importações necessárias, se houver (Dependências)
+  templateUrl: './terminal.html', // Caminho do arquivo HTML
+  styleUrl: './terminal.scss' // Caminho do arquivo SCSS
 })
 export class Terminal {
 

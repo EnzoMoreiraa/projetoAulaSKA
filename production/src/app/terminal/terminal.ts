4cdebcae196194b2e8f09973c2097f7aa09b6d75
unknown <enzomoreiradasilveira@gmail.com>
2025-10-07 16:28:19 -0300

    Update project

--- production/src/app/terminal/terminal.ts
+++ production/src/app/terminal/terminal.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { ProductionControl } from '../production-control/production-control';
 
 @Component({
   selector: 'app-terminal', // Nome do componente
   standalone: true, // Componente independente
-  imports: [], // Importações necessárias, se houver (Dependências)
+  imports: [ProductionControl], // Importações necessárias, se houver (Dependências)
   templateUrl: './terminal.html', // Caminho do arquivo HTML
   styleUrl: './terminal.scss' // Caminho do arquivo SCSS
 })

f467bb99fa327d50cca86f49dae1c20b912bbe7d
unknown <enzomoreiradasilveira@gmail.com>
2025-10-14 16:28:56 -0300

    Updating projects

--- production/src/app/terminal/terminal.ts
+++ production/src/app/terminal/terminal.ts
@@ -2,6 +2,7 @@ import { Component, inject } from '@angular/core';
 import { ProductionControl } from '../production-control/production-control';
 import { MatDialog, MatDialogRef, MatDialogModule } from '@angular/material/dialog';
 import { DialogSelect } from '../dialog-select/dialog-select';
+import productionOrders from '../../assets/files/production-orders.json';
 
 @Component({
   selector: 'app-terminal', // Nome do componente
@@ -14,6 +15,8 @@ export class Terminal {
   
   readonly dialog: MatDialog = inject(MatDialog);
 
+  productionOrders: any[] = productionOrders
+
   setProductionOrder() {
     console.log('Production order set!');
     this.openSelectDialog();
@@ -23,7 +26,7 @@ export class Terminal {
     const dialogRef: MatDialogRef<DialogSelect, any> = this.dialog.open(DialogSelect, {
       width: '950px',
       panelClass: 'custom-dialog',
-      data: { dialogTitle: 'Teste dialog' },
+      data: { dialogTitle: 'Teste dialog', optionsList: this.productionOrders }
     });
 
     return new Promise((resolve) => {

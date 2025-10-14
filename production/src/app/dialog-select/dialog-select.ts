42cc42ffbfe1d9336aa156a1e40dd60244a2c419
unknown <enzomoreiradasilveira@gmail.com>
2025-10-14 15:09:42 -0300

    Pinter

---
+++ production/src/app/dialog-select/dialog-select.ts
@@ -0,0 +1,16 @@
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef, MatDialogModule } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-dialog-select',
+  imports: [],
+  templateUrl: './dialog-select.html',
+  styleUrl: './dialog-select.scss'
+})
+export class DialogSelect {
+  dialogTitle: string = '';
+
+  constructor(public dialogRef: MatDialogRef<DialogSelect>, @Inject(MAT_DIALOG_DATA) public data: any) {
+    this.dialogTitle = data.dialogTitle;
+  }
+}
--- production/src/app/terminal/terminal.ts
+++ production/src/app/terminal/terminal.ts
@@ -1,13 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { ProductionControl } from '../production-control/production-control';
+import { MatDialog, MatDialogRef, MatDialogModule } from '@angular/material/dialog';
+import { DialogSelect } from '../dialog-select/dialog-select';
 
 @Component({
   selector: 'app-terminal', // Nome do componente
   standalone: true, // Componente independente
   imports: [ProductionControl], // Importações necessárias, se houver (Dependências)
   templateUrl: './terminal.html', // Caminho do arquivo HTML
-  styleUrl: './terminal.scss' // Caminho do arquivo SCSS
+  styleUrl: './terminal.scss', // Caminho do arquivo SCSS
 })
 export class Terminal {
+  
+  readonly dialog: MatDialog = inject(MatDialog);
 
+  setProductionOrder() {
+    console.log('Production order set!');
+    this.openSelectDialog();
+  }
+
+  openSelectDialog(): any {
+    const dialogRef: MatDialogRef<DialogSelect, any> = this.dialog.open(DialogSelect, {
+      width: '950px',
+      panelClass: 'custom-dialog',
+      data: { dialogTitle: 'Teste dialog' },
+    });
+
+    return new Promise((resolve) => {
+      dialogRef.afterClosed().subscribe((result) => {
+        resolve(result);
+      });
+    });
+  }
 }

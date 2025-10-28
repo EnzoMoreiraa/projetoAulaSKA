ba4fed789cdeeeea90613c9a71c4c6c77df96454
unknown <enzomoreiradasilveira@gmail.com>
2025-10-28 13:59:52 -0300

    mateus

--- production/src/app/production-control/production-control.ts
+++ production/src/app/production-control/production-control.ts
@@ -1,4 +1,6 @@
 import { Component, Input } from '@angular/core';
+import { ProductionStatus } from '../../../../common/ProductionStatus';
+import { ProductionStatus_e, ProductionStatusColor_e } from '../../../../common/enums/enum';
 
 @Component({
   selector: 'app-production-control',
@@ -8,4 +10,14 @@ import { Component, Input } from '@angular/core';
 })
 export class ProductionControl {
   @Input() title: string = 'Indefinido';
+
+  @Input() icon: string | undefined;
+  @Input() productionOrderQuantity: number | undefined;
+  @Input() productionStatus: ProductionStatus | any;
+  @Input() productionOrderKey: string | any;
+
+  value: number = 0;
+  valuePercentage: number = 0;
+  ProductionStatus_e: typeof ProductionStatus_e = ProductionStatus_e;
+  ProductionStatusColor_e: typeof ProductionStatusColor_e = ProductionStatusColor_e;
 }
